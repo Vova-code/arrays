90bb85b7b630f15b8ac82c5565b848bdada7fcf9
Vova <vdoche@live.fr>
2021-10-27 21:09:23 +0200

    Resolve exercice 9

--- src/exercise-9.js
+++ src/exercise-9.js
@@ -1,4 +1,20 @@
 // Same as 8 but use .flat() and flatten 2 levels instead of 1
 // e.g. fn([[1, 2], [3, 4, [5], [[6]]]) === [1, 2, 3, 4, 5, [6]]
 
-module.exports = () => {};
+module.exports = (array) => {
+  let expectedArray = [];
+
+  if (!array) {
+    throw new Error("Invalid argument");
+  }
+
+  array.forEach((element) => {
+    if (Array.isArray(element)) {
+      expectedArray.push(element.flat());
+    } else {
+      expectedArray.push(element);
+    }
+  });
+
+  return expectedArray.flat();
+};
