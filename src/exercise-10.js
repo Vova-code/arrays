6ae615a9d3c4b0e812ef5be309ee9858fb6bd484
Vova <vdoche@live.fr>
2021-10-27 22:23:41 +0200

    Resolve exercice 10

--- src/exercise-10.js
+++ src/exercise-10.js
@@ -1,3 +1,13 @@
 // Use map() and flat() to extract all movies' id in a flat array.
 
-module.exports = () => {};
+module.exports = (array) => {
+  if (!array) {
+    throw new Error("Invalid argument");
+  }
+
+  let expectedArray = [];
+
+  array.map(({ videos }) => expectedArray.push(videos));
+
+  return expectedArray.flat().map(({ id }) => id);
+};
